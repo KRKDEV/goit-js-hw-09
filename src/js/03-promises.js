cc053588d252ce5cbe239766f4e8c4964ed6359e
KRKDEV <111200477+KRKDEV@users.noreply.github.com>
2023-01-31 18:16:31 +0100

    Task 3 - 100% done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,30 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const delayInput = document.querySelector('[name="delay"]');
+const stepInput = document.querySelector('[name="step"]');
+const amountInput = document.querySelector('[name="amount"]');
+const submitBtn = document.querySelector('button');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
     // Fulfill
+    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
   } else {
     // Reject
+    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
   }
 }
+
+submitBtn.addEventListener('click', event => {
+  event.preventDefault();
+  let currentDelay = parseInt(delayInput.value);
+  setTimeout(() => {
+    for (let i = 0; i < amountInput.value; i++) {
+      setTimeout(() => {
+        createPromise(i + 1, currentDelay), parseInt(stepInput.value);
+        currentDelay = currentDelay + parseInt(stepInput.value);
+      }, parseInt(stepInput.value) * i);
+    }
+  }, parseInt(delayInput.value));
+});
