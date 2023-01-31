707f763465605699c148126712b575fc8bb0bafe
KRKDEV <111200477+KRKDEV@users.noreply.github.com>
2023-01-31 15:38:30 +0100

    Task 1 - 100%

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const body = document.querySelector('body');
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let timer = null;
+startButton.addEventListener('click', () => {
+  startButton.disabled = true;
+  stopButton.disabled = false;
+  body.style.backgroundColor = `${getRandomHexColor()}`;
+  timer = setInterval(() => {
+    let randomColor = getRandomHexColor();
+    body.style.backgroundColor = `${randomColor}`;
+  }, 1000);
+});
+
+stopButton.addEventListener('click', () => {
+  stopButton.disabled = true;
+  startButton.disabled = false;
+  clearInterval(timer);
+});
